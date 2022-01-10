de0c9bc87ad1a14206a5c6f11078852d00baeff2
crocka <chino123abc@hotmail.com>
2022-01-10 21:56:29 +0000

    use template strings

--- assertEqual.js
+++ assertEqual.js
@@ -3,13 +3,11 @@ const assertEqual = function(actual, expected) {
 
   if (actual !== expected) {
 
-    console.log('❌❌❌ Assertion Failed: ' + actual +
-    ' !== ' + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
 
   } else if (actual === expected) {
 
-    console.log('✅ ✅ ✅ Assertion Passed: ' + actual +
-    ' === ' + expected);
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}.`);
   }
 
 };
