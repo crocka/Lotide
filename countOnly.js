0b5a51edcd602fb5efdaf8c55f6257de445c61db
crocka <chino123abc@hotmail.com>
2022-01-12 21:19:39 +0000

    implement countOnly

---
+++ countOnly.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
+
+  } else if (actual === expected) {
+
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}.`);
+  }
+
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+
+  const results = {};
+  
+  for (const item of allItems) {
+
+    itemsToCount[item] ? results[item] ? results[item] += 1 : results[item] = 1 : '';
+
+  }
+  
+  return results;
+
+};
