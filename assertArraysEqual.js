d5493c8289176b847383e065a39eb3ab5a79d744
crocka <chino123abc@hotmail.com>
2022-01-12 00:38:58 +0000

    implement assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
-
-  } else if (actual === expected) {
-
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}.`);
-  }
-
-};
-
 const eqArrays = function(array1, array2) {
 
   let equal;
@@ -29,16 +16,6 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function (array1, array2) {
 
-  let equal;
-
-  for (let i = 0; i < array1.length; i++){
-
-    equal = array1[i] === array2[i];
-
-    equal ? '' : equal = false;
-
-  }
-
-  equal ? console.log(`✅ ✅ ✅ Assertion Passed: ${array1} === ${array2}.`) : console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}.`);
+  eqArrays(array1, array2) ? console.log(`✅ ✅ ✅ Assertion Passed: ${array1} === ${array2}.`) : console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}.`);
 
 };
